05b4abbc36baa6d800baba21a68c6e9a41e9113a
ims0s <soruor2001@gmail.com>
2022-10-25 01:15:14 +0200

    put comments for making it understandable.

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 function computer(){
-let Cchoise=Math.floor(Math.random()*10)%3;
-    return Cchoise
+    let Cchoise=Math.floor(Math.random()*10)%3; //generate random numbre from 0 to 2 
+    return Cchoise // return  a number as 1 --> rock 
+                                       // 2 --> paper
+                                       // 3 --> scissor
 }
 function player(){
     while(true){
-        let choise=prompt('rock, paper or scissors ');
-
+        let choise=prompt('rock, paper or scissor ');
+        
+        //swap the player choice to a number  
         if(choise.toLowerCase() == 'rock' ){
-            return 0; 
+            return 0; // 0 means rock
         }else if(choise.toLowerCase() == 'paper' ){
-            return 1;
+            return 1; // 1 means paper
         }else if(choise.toLowerCase() == 'scissor' ){
-            return 2
+            return 2 // 2 means scissor
         }else{
-            console.warn('your choise is not correct')
+            console.warn('your choise is not correct') // this for tell the player is writing
+                                                        // a wrong choise  and keeping the loop turned.
         }
     }
     
 }
 function playground(player , comp){
+    // here i made the game tell who's the winner or if there is a tie
     if(player == comp){
         return 'Tie';
     }else if(player==0&&comp==3){
@@ -36,15 +41,15 @@ function playground(player , comp){
 let compScore=0;
 let playerScore=0;
 for(let i =0;i<5;i++){
-    
+    //the game of 5 rounds
     let winner=playground(player(),computer());
-    if(winner == 'Tie'){
+    if(winner == 'Tie'){ // this for tie 
         compScore++;
         playerScore++;
-    }else if(winner == 'player'){
+    }else if(winner == 'player'){ // point for player
         playerScore++;
-    }else if(winner == 'comp'){
+    }else if(winner == 'comp'){ // point for computer
         compScore++;
     }
-    console.log(`Player: ${playerScore} - Computer: ${compScore}`)
+    console.log(`Player: ${playerScore} - Computer: ${compScore}`) // print the score 
 }
